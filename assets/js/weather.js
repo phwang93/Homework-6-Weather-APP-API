42a619ff8f488b290f865fe71fbfdc214a9ebb38
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-04-30 19:58:51 -0700

    humidity added

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -18,20 +18,51 @@ function InfoContainer() {
         
         //Getting the temps(min,max) for each day
         for(i =0; i < 5; i++) {
-            document.getElementById("day" + (i + 1) + "Low").innerHTML = "Low: " + Number(data.list[i].main.temp_min).toFixed(1) + "°";
+            // Transforming temp K to F - Low Temp
+            var lowTemp = data.list[i].main.temp_min;
+            console.log(lowTemp);
+            var flowTemp = ((lowTemp-273.15)*1.8)+32
+            console.log(flowTemp)
+            
+            
+            // Displaying the High Temp for each day
+            
+            document.getElementById("day" + (i + 1) + "Low").innerHTML = "Low: " + Number(flowTemp).toFixed(1) + "°";
         }
 
         for(i =0; i < 5; i++) {
-            document.getElementById("day" + (i + 1) + "High").innerHTML = "High: " + Number(data.list[i].main.temp_max).toFixed(1) + "°";
+
+             // Transforming temp K to F - High Temp
+
+              var highTemp = data.list[i].main.temp_max;
+               console.log(highTemp);
+               var fhighTemp = ((highTemp-273.15)*1.8)+32
+                console.log(fhighTemp)
+
+             // Displaying the High Temp for each day
+
+                document.getElementById("day" + (i + 1) + "High").innerHTML = "High: " + Number(fhighTemp).toFixed(1) + "°";
         }
 
-        //display weather icons from API, depending on conditions
+        //Gather and display weather icons from API, depending on conditions
         for (i = 0; i < 5; i++){
             document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/" + 
             data.list[i].weather[0].icon + ".png";
         }
+       
+        // Gather and display humidity
+        for (i = 0; i < 5; i++){
+            document.getElementById("day" + (i + 1) + "Humid").innerHTML = "Humidity: " + Number(data.list[i].main.humidity).toFixed(1) + "%";
+
+        }
+        // Gather and display windspeed
+        for (i = 0; i < 5; i++){
+        }
+
+
+
     })
-    
+
     //if error occurs, display error alert
     .catch(err => alert("The weather is not available"))
 }
@@ -52,5 +83,5 @@ function CheckDay(day){
 
 for (i = 0; i < 5; i++) {
     document.getElementById("day" + (i + 1)).innerHTML = weekDay[CheckDay(i)];
-
 }
+
